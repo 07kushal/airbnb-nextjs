3d5f4c39cc1d8cac4d62f3c1c948244d90b9794c
07kushal <saini.kushal2@gmail.com>
2023-08-05 01:03:20 +0530

    INFO step is completed of Airbnb your home! Flow.

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
   CATEGORY = 0,
@@ -46,9 +47,13 @@ const RentModal = () => {
 
   const category = watch("category");
   const location = watch("location");
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const guestCount = watch("guestCount");
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
+
   const Map = useMemo(
     () => dynamic(() => import("../Map"), { ssr: false }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [location],
   );
 
@@ -62,15 +67,12 @@ const RentModal = () => {
 
   const onBack = () => {
     setStep((value) => {
-      console.log("onBack value", value);
       return value - 1;
     });
   };
 
   const onNext = () => {
     setStep((value) => {
-      console.log("onNext value", value);
-
       return value + 1;
     });
   };
@@ -139,11 +141,42 @@ const RentModal = () => {
     );
   }
 
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenitis do you have?"
+        />
+        <Counter
+          onChange={(value) => setCustomValue("guestCount", value)}
+          value={guestCount}
+          title="Guests"
+          subtitle="How many guests do you allow?"
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue("roomCount", value)}
+          value={roomCount}
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue("bathroomCount", value)}
+          value={bathroomCount}
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+        />
+      </div>
+    );
+  }
+  console.log("step", step);
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={step < STEPS.INFO ? onNext : () => {}}
       title="Airbnb your home!"
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
