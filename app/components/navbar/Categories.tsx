be18f9bcb53ddee2bf31ee2ccdf36efa9b09f3fc
kushal saini <saini.kushal2@gmail.com>
2023-08-04 20:37:07 +0530

    Categories UI added. On Click Handler added on Navbar via Next Router.

---
+++ app/components/navbar/Categories.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Container from "../Container";
+import { categories } from "../../constants";
+import CategoryBox from "../CategoryBox";
+import { usePathname, useSearchParams } from "next/navigation";
+
+const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathName = usePathname();
+  const isMainPage = pathName === "/";
+  if (!isMainPage) {
+    return null;
+  }
+  return (
+    <Container>
+      <div
+        className="
+        pt-4
+        flex
+        flex-row
+        items-center
+        justify-between
+        overflow-x-auto"
+      >
+        {categories.map((item) => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+            selected={category === item.label}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Categories;
