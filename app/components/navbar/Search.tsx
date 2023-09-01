7fc56a131bbdc5f460f927a1b70162e31953b877
07kushal <saini.kushal2@gmail.com>
2023-09-01 17:06:11 +0530

    Search params added on search bar.

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,13 +1,55 @@
 "use client";
 
+import useCountries from "@/app/hooks/useCountries";
 import useSearchModal from "@/app/hooks/useSearchModal";
+import { differenceInDays } from "date-fns";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
+import { start } from "repl";
 
 const Search = () => {
   const searchModal = useSearchModal();
-  const locationLabel = "Anywhere";
-  const durationLabel = "Any Week";
-  const guestLabel = "Add Guests";
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const locationValue = params?.get("locationValue");
+  const startDate = params?.get("startDate");
+  const endDate = params?.get("endDate");
+  const guestCount = params?.get("guestCount");
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+    return "Anywhere";
+  }, [getByValue, locationValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return "Any Week";
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return "Add Guests";
+  }, [guestCount]);
+
   return (
     <div
       onClick={searchModal.onOpen}
